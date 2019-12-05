bb33761f9b4c4b8d523a3f02890d624290540f82
Florent <grenaille.florent85@gmail.com>
2019-12-05 19:53:09 +0100

    Update webhook for print the name the user request

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ app.post('/webhook', (req, res) => {
         // Get the sender PSID
         let sender_psid = webhook_event.sender.id;
         console.log('Sender PSID: ' + sender_psid);
+        console.log('Sender name: ' + webhook_event.sender.name);
 
         // Check if the event is a message or postback and
         // pass the event to the appropriate handler function
