77f5ed9c521b45f336c963c5630e2b88bfbd5568
Florent <grenaille.florent85@gmail.com>
2019-12-05 16:35:06 +0100

    Code refacto

---
+++ manager_events/manager_event_message.js
@@ -0,0 +1,48 @@
+// import the 'callSendApi' function that into the 'send_api.js'
+const sendApi = require('../send_message');
+
+// Handles messages events
+exports.handleMessage = (sender_psid, received_message) => {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachments!`
+        };
+    } else if (received_message.attachments) {
+
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+
+    // Sends the response message
+    sendApi.callSendAPI(sender_psid, response);
+}
