e7e0be43945d5acde3718cf61c97a5525dc17292
Florent <grenaille.florent85@gmail.com>
2019-12-05 18:24:41 +0100

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,15 +1,15 @@
 exports.parser = (text) => {
+    let date = new Date();
+    let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+    let month = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+
     if (text.includes("jour")) {
+        return `Aujourd'hui nous somme le ${days[date.getDay() - 1]}`;
 
-        let date = new Date();
-        let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
-        let month = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
-
+    } else if (text.includes("date")) {
         return `Aujourd'hui nous somme le ${days[date.getDay() - 1]} ${date.getDate()} ${month[date.getMonth()]}`;
 
     } else {
-
-        return `Votre message est le suivant: ${text}\nVoici la liste de commande:\n - ... jour...\n - ... date...`;
-
+        return `Votre message est le suivant: ${text}\nVoici la liste des mots reconnus dans une phrase:\n - ... jour...\n - ... date...`;
     }
 };
