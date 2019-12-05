67f79463a0db5786f7f06e26f9fd5f98ef958ab2
Florent <grenaille.florent85@gmail.com>
2019-12-05 18:03:29 +0100

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,7 +1,14 @@
 exports.parser = (text) => {
-    if (text.includes("jours")) {
-        return `Aujourd'hui nous somme le ${Date()}`;
+    if (text.includes("jour")) {
+
+        let date = new Date();
+        let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+        return `Aujourd'hui nous somme le ${days[date.getDay() - 1]} ${date.getDate()}`;
+
     } else {
-        return `Votre message est le suivant: ${text}`;
+
+        return `Votre message est le suivant: ${text}\nVoici la liste de commande:\n - ... jour...\n - ... date...`;
+
     }
 };
